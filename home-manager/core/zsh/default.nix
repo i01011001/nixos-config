4a2df76de522312533428983c57535ccb873d2b9
yugen <yugen.m7@gmail.com>
2025-01-29 22:49:14 +0545

    added command for zephyr

--- home-manager/core/zsh/default.nix
+++ home-manager/core/zsh/default.nix
@@ -10,6 +10,7 @@
       update = "sudo nixos-rebuild switch --verbose --offline && rm -rf ~/.cache/tofi-drun";
       source_idf = "source $HOME/code/esp/idf/esp-idf/export.sh";
       source_matter = "source $HOME/code/esp/idf/esp-matter/export.sh";
+      source_zephyr= "source $HOME/code/zephyrproject/.venv/bin/activate && source $HOME/code/zephyrproject/zephyr/zephyr-env.sh && source $HOME/code/zephyrproject/zephyr/.west-completion.zsh";
       nixc = "cd /etc/nixos/ && nvim .";
     };
     history = {
